0a719edd31f1e90a8609c6322841910377a0f450
Felipe Lungov <felipe@notmyemail.com>
2020-01-18 19:51:58 -0300

    everything looks right v0.1.0

--- cnc-check-cron-queue.php
+++ cnc-check-cron-queue.php
@@ -11,16 +11,22 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-function activate() {
+register_activation_hook( __FILE__, 'cnc_ccq_activation' );
+register_deactivation_hook( __FILE__, 'cnc_ccq_deactivation' );
+add_action( 'wp_dashboard_setup', 'cnc_ccq_create_dashboard_widget' );
+add_action( 'cnc_ccq_check_cron_queue', 'cnc_ccq_check_cron_queue' );
+
+function cnc_ccq_activation() {
     cnc_ccq_schedule_next_check();
 }
 
-function deactivate() {
+function cnc_ccq_deactivation() {
     wp_clear_scheduled_hook( 'cnc_ccq_check_cron_queue' );
 }
 
 function cnc_ccq_schedule_next_check(){
     $next_run = time() + (12*60*60) + rand(0, 1*60*60); // anything between 12 and 13 hours from now
+    // $next_run = time() + 7; // debug
     wp_schedule_single_event( $next_run, 'cnc_ccq_check_cron_queue' );
 }
 
@@ -28,7 +34,7 @@ function cnc_ccq_check_cron_queue(){
     
     $cron_events = get_option( 'cron' );
     $overdue_events_history = get_option( 'cnc_ccq_overdue_events_history' );
-    if( 50 < count( $overdue_events_history ) ){
+    if( $overdue_events_history && 50 <= count( $overdue_events_history ) ){
         array_shift( $overdue_events_history );
     }
 
@@ -36,7 +42,7 @@ function cnc_ccq_check_cron_queue(){
     $overdue_events = 0;
     $longest_delay = 0;
     foreach( $cron_events as $event_time => $event_info ){
-        if( $now > $event_time ) {
+        if( 'version' !== $event_time && $now > $event_time ) {
             $overdue_events++;
             $longest_delay = max( $longest_delay, $now - $event_time );
         }
@@ -55,40 +61,47 @@ function cnc_ccq_check_cron_queue(){
 function cnc_ccq_create_dashboard_widget(){
     wp_add_dashboard_widget( 'cnc_check_cron_queue', __('Info regarding your WP Cron events'), 'cnc_ccq_dashboard' );
 }
-add_action( 'wp_dashboard_setup', 'cnc_ccq_create_dashboard_widget' );
 
 function cnc_ccq_dashboard(){
 
     $overdue_events_history = get_option( 'cnc_ccq_overdue_events_history' );
 
-    $highest_overdue = 0;
-    $total_overdue = 0;
-    $longest_delay = 0;
-    $records_no_overdue = 0;
-    $records_total = 0;
-    $last_check = null;
+    if ( $overdue_events_history ){
 
-    foreach( $overdue_events_history as $record_time => $record_info ){
+        $highest_overdue = 0;
+        $total_overdue = 0;
+        $longest_delay = 0;
+        $records_no_overdue = 0;
+        $records_total = 0;
+        $last_check = null;
 
-        $highest_overdue = max( $highest_overdue, $record_info['overdue_events'] ); // integer
-        $total_overdue += $record_info['overdue_events']; // integer
-        $longest_delay = max( $longest_delay, $record_info['longest_delay'] ); // integer (seconds)
-        if( 0 === $record_info['overdue_events'] ) { $records_no_overdue++ }; // integer
-        $records_total++; // integer
-        $last_check = max( $last_check, $record_time ); // time
+        foreach( $overdue_events_history as $record_time => $record_info ){
 
+            $highest_overdue = max( $highest_overdue, $record_info['overdue_events'] ); // integer
+            $total_overdue += $record_info['overdue_events']; // integer
+            $longest_delay = max( $longest_delay, $record_info['longest_delay'] ); // integer (seconds)
+            if( 0 === $record_info['overdue_events'] ) { $records_no_overdue++; } // integer
+            $records_total++; // integer
+            $last_check = max( $last_check, $record_time ); // time
+
+        }
+
+        if( 0 !== $records_total ){
+            $average_overdue = round( $total_overdue / $records_total, 2 ); // decimal
+            $percentage_no_overdue = ( 100 * round( $records_no_overdue / $records_total, 2 ) ) . '%'; // string (percentage)
+        }
+        $longest_delay = round( $longest_delay / 60, 2 ); // decimal
+        $last_check = date( 'Y-m-d H:i:s', $last_check );
+
+        echo( "<p>Highest number of overdue events: $highest_overdue</p>" );
+        echo( "<p>Average number of overdue events: $average_overdue</p>" );
+        echo( "<p>Longest delay of an overdue event: $longest_delay minutes</p>" );
+        echo( "<p>Frequency of no overdue events: $percentage_no_overdue</p>" );
+        echo( "<p>Last check: $last_check</p>" );
+
+    }else{
+        echo( '<p>No data to display.</p>' );
     }
-
-    $average_overdue = round( $total_overdue / $records_total, 2 ); // decimal
-    $longest_delay = round( $longest_delay / 60, 2 ); // decimal
-    $percentage_no_overdue = ( 100 * round( $records_no_overdue / $records_total, 2 ) ) . '%'; // string (percentage)
-    $last_check = date( 'Y-m-d H:i:s', $last_check );
-
-    echo( "<p>Highest number of overdue events: $highest_overdue</p>" );
-    echo( "<p>Average number of overdue events: $average_overdue</p>" );
-    echo( "<p>Longest delay of an overdue event: $longest_delay minutes</p>" );
-    echo( "<p>Frequency of no overdue events: $percentage_no_overdue</p>" );
-    echo( "<p>Last check: $last_check</p>" );
 
 }
 
